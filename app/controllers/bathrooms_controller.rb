89095670d76fb8484bb9e985813405091342119f
narriteration <narriteration@gmail.com>
2017-04-27 13:37:35 -0700

    bathrooms added to location show view. bathroom show working

--- app/controllers/bathrooms_controller.rb
+++ app/controllers/bathrooms_controller.rb
@@ -1,26 +1,47 @@
 class BathroomsController < ApplicationController
 
+
   def index
     @bathrooms = Bathroom.all
   end
 
+
   def new
     @bathroom = Bathroom.new
     @locations = Location.all
-    #form for user to create a new bathroom
-    # MUST have a location to create and save to db
   end
 
-  def create
 
+  def create
     @locations = Location.all
     bathroom_params = params.require(:bathroom).permit(:location_description, :is_gendered, :is_fam_friendly, :is_accessible, :rating, :location_id)
     bathroom = Bathroom.new(bathroom_params)
-
     if bathroom.save
-      redirect_to bathrooms_path #TODO: update to show page for locations/:id/bathrooms/:id
+      redirect_to bathrooms_path
     end
+  end
 
+
+  def show
+    bathroom_id = params[:id]
+    @bathroom = Bathroom.find_by(id: bathroom_id)
+    location_id = params[:location_id]
+    @location = Location.find_by(id: location_id)
+  end
+
+
+  def edit
+    #
+  end
+
+
+  def update
+    #
+  end
+
+
+  def destroy
+    #
   end
 
 end
