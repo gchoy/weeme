49c3c72f864be7c8f13c76e728748bd8a86f32cb
narriteration <narriteration@gmail.com>
2017-04-30 15:33:24 -0700

    added static map onto locations index that plots every location in our DB

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -6,7 +6,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "AIzaSyCtP4MzO49Ht9H4q4nPFRi9fLvThHVOHwA"               # API key for geocoding service
+  api_key: "AIzaSyCtP4MzO49Ht9H4q4nPFRi9fLvThHVOHwA",               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
@@ -16,6 +16,6 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  # units: :mi,                 # :km for kilometers or :mi for miles
+  units: :mi               # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
