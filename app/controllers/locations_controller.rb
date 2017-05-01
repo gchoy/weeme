36e37d928c30005d53c7af6b299a74b4ec4f9e7a
gchoy <gabriela.choy@gmail.com>
2017-04-30 20:30:23 -0700

    Added getting directions with google

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -13,7 +13,7 @@ class LocationsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
     marker.lat location.latitude
     marker.lng location.longitude
-    marker.infowindow location.name
+    marker.infowindow "<a target=location.name href='https://www.google.com/maps/place/"+"#{location.full_address}"+"'>Get Directions With Google Maps</a>"
     end
 end
 
