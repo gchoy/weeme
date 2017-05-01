2d9a711b4f422003dfc74e1a706ffc8940200e10
ben-manning <bmanning@me.com>
2017-05-01 15:06:15 -0700

    hopefully fixed the migration issues

---
+++ db/migrate/20170501220309_delete_and_recreate_bathroom.rb
@@ -0,0 +1,13 @@
+class DeleteAndRecreateBathroom < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bathrooms do |t|
+     t.string :location_description
+     t.boolean :is_fam_friendly
+     t.boolean :is_gendered
+     t.boolean :is_accessible
+     t.float :rating
+
+     t.timestamps
+   end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427214209) do
+ActiveRecord::Schema.define(version: 20170501220451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bathrooms", force: :cascade do |t|
+    t.string   "location_description"
+    t.boolean  "is_fam_friendly"
+    t.boolean  "is_gendered"
+    t.boolean  "is_accessible"
+    t.float    "rating"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "location_id"
+    t.index ["location_id"], name: "index_bathrooms_on_location_id", using: :btree
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -48,4 +60,5 @@ ActiveRecord::Schema.define(version: 20170427214209) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bathrooms", "locations"
 end
