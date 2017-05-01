d15fda02f9150948df53cc2dbbc7e72915496c39
gchoy <gabriela.choy@gmail.com>
2017-04-30 19:56:41 -0700

    Info window added to markers on map

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -13,6 +13,7 @@ class LocationsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
     marker.lat location.latitude
     marker.lng location.longitude
+    marker.infowindow location.name
     end
 end
 
